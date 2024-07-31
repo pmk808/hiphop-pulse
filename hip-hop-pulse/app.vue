8377e13ca3c74025c1e9aae2e8ede1a2729158f5
Peemak <cabanesmcphy123@gmail.com>
2024-07-31 18:28:05 +0800

    init

--- hip-hop-pulse/app.vue
+++ hip-hop-pulse/app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="bg-black min-h-screen">
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
--- hip-hop-pulse/pages/index.vue
+++ hip-hop-pulse/pages/index.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="container mx-auto p-4">
-      <h1 class="text-3xl font-bold mb-4">Home Page</h1>
-      <p class="text-lg">Welcome to the Home page of Hip Hop Pulse!</p>
-      <!-- Use the AboutPage component here -->
-      <AboutPage />
-    </div>
-  </template>
-  
-  <script setup>
-  import AboutPage from './about.vue'
-  </script>
-  
-  <style>
-  /* Add your styles here */
-  </style>
+  <div class="hero-section bg-cover bg-center h-screen flex flex-col justify-center items-center text-center text-white">
+    <h1 class="hero-title text-6xl mb-4">Hip-Hop Pulse</h1>
+    <p class="hero-tagline text-2xl text-red-500">Culture Beyond the Beat</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+.hero-title {
+  font-family: 'Staatliches', sans-serif;
+}
+
+.hero-tagline {
+  font-family: 'Bebas Neue', sans-serif;
+}
+</style>
